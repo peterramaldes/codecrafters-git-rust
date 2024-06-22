69bab685e2c68ab516e5b51e89879aecb0bd7608
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2024-06-22 11:17:39 -0300

    Structure better the commands/subcommands.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
-use std::env;
+use clap::{Parser, Subcommand};
 use std::fs;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+#[derive(Subcommand)]
+enum Commands {
+    /// Initialize a git repository
+    Init,
+}
+
+fn main() {
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Init => init(),
     }
+}
+
+fn init() {
+    fs::create_dir(".git").unwrap();
+    fs::create_dir(".git/objects").unwrap();
+    fs::create_dir(".git/refs").unwrap();
+    fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+    println!("Initialized git directory")
 }
